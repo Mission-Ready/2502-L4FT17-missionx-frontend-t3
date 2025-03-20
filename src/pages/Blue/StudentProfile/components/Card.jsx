21015a0f4929a0faf2d9a7801024c569267140d0
Eugene Lee <eugenel@missionreadyhq.com>
2025-03-20 23:38:50 +1300

    added sidebar

---
+++ src/pages/Blue/StudentProfile/components/Card.jsx
@@ -0,0 +1,14 @@
+import styles from "./Card.module.css";
+
+export default function Card({ name, image }) {
+  return (
+    <div className={styles.card}>
+      <img
+        className={styles.image}
+        src={image}
+        alt={name}
+      />
+      <p className={styles.name}>{name}</p>
+    </div>
+  );
+}
