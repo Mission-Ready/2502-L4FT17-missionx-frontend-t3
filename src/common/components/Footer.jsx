eeaa431a77cc931de902688f800c2660df277ab8
Takashi Murachi <takashim@missionreadyhq.com>
2025-03-22 14:36:41 +1300

    Add: Brown project submission functionality with student pronouns

---
+++ src/common/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '.components/Footer.module.css'; // CSSモジュールを使用する場合
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <p>© LevelUp Works 2020</p>
+      </div>
+    </footer>
+  );
+}
+
+
---
+++ src/pages/Brown/ProjectSubmission/studentProjectsData.js
@@ -0,0 +1,19 @@
+const studentProjectsData = [
+  {
+    id: 1,
+    student_id: 23,
+    project_id: 10,
+    dates: {
+      date_started: "2020-12-31 23:59:59",
+      date_submitted: "2020-12-31 23:59:59",
+      date_completed: "2020-12-31 23:59:59",
+    },
+    submissions: [
+      "https://cdn.filestackcontent.com/VfOA9GyQCO6oZpOvroAi",
+    ],
+    submission_status: true,
+  },
+];
+
+export default studentProjectsData;
+  
