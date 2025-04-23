56afd6b6cbd700072e18384c84e6b6ec27dbf094
Solomone <Solomonel@missionhq.com>
2025-04-23 12:05:24 +1200

    update teacher profile button in header component

--- src/pages/Turqoise/TeacherProfileViewer/TeacherProfileViewer.jsx
+++ src/pages/Turqoise/TeacherProfileViewer/TeacherProfileViewer.jsx
@@ -1,15 +1,9 @@
 import MoneHeader from "./components/MoneHeader";
-import MoneFooter from "./components/MoneFooter";
-import Profile from "./components/Profile";
-import Linkbtn from "./components/Linkbtn";
 
 function teacherProfile() {
   return (
     <div>
       <MoneHeader />
-      <Profile />
-      <Linkbtn />
-      <MoneFooter />
     </div>
   );
 }
